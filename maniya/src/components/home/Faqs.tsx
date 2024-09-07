31d389c743c9e88508a2100c4545b3684e20f7b8
Jayjeet1013 <jayjeet1013@gmail.com>
2024-09-07 17:41:06 +0530

    update

---
+++ maniya/src/components/home/Faqs.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Link from "next/link";
+
+interface Question {
+  question: string;
+  answer: string;
+}
+
+interface QuestionItemProps {
+  question: string;
+  answer: string;
+}
+
+const questions = [
+  {
+    question: "How do I get started?",
+    answer:
+      "Visit the Maniya Bot website, create your account, and connect your Web3 wallet to start messaging securely using blockchain technology.",
+  },
+  {
+    question: "Is Maniya Bot secure?",
+    answer:
+      "Yes, Maniya Bot uses blockchain technology to ensure all messages are encrypted and stored securely on the blockchain, providing enhanced privacy and security.",
+  },
+  {
+    question: "Are there any transaction fees?",
+    answer:
+      "Maniya Bot allows you to message for free. However, you may incur minimal gas fees for blockchain transactions like storing messages on-chain.",
+  },
+  {
+    question: "How can I reach customer support?",
+    answer:
+      "You can reach out to Maniya's support team through the contact form on the website or by emailing us directly.",
+  },
+];
+
+const QuestionItem: React.FC<QuestionItemProps> = ({ question, answer }) => (
+  <div className="flex items-start">
+    <div className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+      <span className="text-lg font-semibold text-white">?</span>
+    </div>
+    <div className="ml-4">
+      <p className="text-xl font-semibold text-white">{question}</p>
+      <p className="mt-4 text-base text-gray-400">{answer}</p>
+    </div>
+  </div>
+);
+
+const Faqs5: React.FC = () => {
+  return (
+    <section className="py-10 bg-[#0d3645] sm:py-16 lg:py-24">
+      <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">
+            Questions & Answers..
+          </h2>
+          <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-200">
+            Explore the common questions and answers about Maniya Bot
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 mt-12 md:mt-20 md:grid-cols-2 gap-y-16 gap-x-20">
+          {questions.map((item, index) => (
+            <QuestionItem key={index} {...item} />
+          ))}
+        </div>
+
+        <div className="flex items-center justify-center mt-12 md:mt-20">
+          <div className="px-8 py-4 text-center bg-gray-800 rounded-full ">
+            <p className="text-gray-50">
+              Didn’t find the answer you need?
+              <Link
+                href="/contact"
+                className="text-yellow-300 transition duration-200 hover:text-yellow-400 focus:text-yellow-400 hover:underline pl-2"
+              >
+                Contact Maniya Support
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Faqs5;
---
+++ maniya/src/components/home/Features.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { FaFingerprint, FaMoon } from "react-icons/fa6";
+import { FiFilter } from "react-icons/fi";
+import { RxLightningBolt } from "react-icons/rx";
+
+const Feature15 = () => {
+  const containerStyles = "py-10 bg-white sm:py-16 lg:py-24";
+  const innerContainerStyles = "px-4 mx-auto max-w-7xl sm:px-6 lg:px-8";
+  const gridStyles =
+    "grid grid-cols-1 text-center sm:grid-cols-2 gap-y-8 lg:grid-cols-4 sm:gap-12";
+  const itemStyles = "flex flex-col items-center";
+
+  return (
+    <section className={containerStyles}>
+      <div className={innerContainerStyles}>
+        <div className={gridStyles}>
+          <div className={itemStyles}>
+            <div className="h-20 w-20 bg-blue-100 flex items-center justify-center rounded-full">
+              <FaFingerprint className="flex items-center justify-center w-8 h-8 mx-auto text-blue-600" />
+            </div>
+
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Secured Messaging
+            </h3>
+            <p className="mt-4 text-sm text-gray-600">
+              Maniya Bot ensures all messages are encrypted and securely stored
+              on the blockchain.
+            </p>
+          </div>
+          <div className={itemStyles}>
+            <div className="h-20 w-20 bg-orange-100 flex items-center justify-center rounded-full">
+              {" "}
+              <RxLightningBolt className="flex items-center justify-center w-8 h-8 mx-auto text-orange-600" />
+            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Fast & Efficient
+            </h3>
+            <p className="mt-4 text-sm text-gray-600">
+              Enjoy fast and efficient communication with Maniya&apos;s
+              blockchain-based messaging.
+            </p>
+          </div>
+          <div className={itemStyles}>
+            <div className="h-20 w-20 bg-green-100 flex items-center justify-center rounded-full">
+              <FaMoon className="flex items-center justify-center w-8 h-8 mx-auto text-green-600" />
+            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Light & Dark Mode
+            </h3>
+            <p className="mt-4 text-sm text-gray-600">
+              Switch between light and dark modes effortlessly with Maniya Bot.
+            </p>
+          </div>
+
+          <div className={itemStyles}>
+            <div className="h-20 w-20 bg-red-100 flex items-center justify-center rounded-full">
+              <FiFilter className="flex items-center justify-center w-8 h-8 mx-auto text-red-600" />
+            </div>
+            <h3 className="mt-8 text-lg font-semibold text-black">
+              Message Filters
+            </h3>
+            <p className="mt-4 text-sm text-gray-600">
+              Easily filter through your conversations to find important
+              messages in Maniya Bot.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Feature15;
